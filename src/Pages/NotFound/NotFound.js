357e7014f86a8b52d345be56643bb2917bec501d
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-11-10 20:06:06 +0600

    Error 404 Page Design

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Error from '../../images/error.PNG';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404 Page Not Found</h2>
-        </div>
+        <>
+            <Container className="text-center my-5 py-5">
+                <img src={Error} className="mt-3 pt-3" alt="" />
+                <h1 className="fw-bold text-danger">404 Page Not Found</h1>
+                <Link to="/">
+                    <button className="btn btn-secondary mt-3">Back to the Home</button>
+                </Link>
+            </Container>
+        </>
     );
 };
 
