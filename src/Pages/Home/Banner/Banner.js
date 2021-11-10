96fc605510dbeeb69950faa9cf0e63b97be83f37
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-11-10 18:01:38 +0600

    Responsive Home Banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import banner from '../../../images/banner.png';
 
 const Banner = () => {
     return (
-        <div>
-            <h2>Home Banner</h2>
-        </div>
+        <>
+            <Container className="my-3">
+                <div className="row gy-5 align-items-center">
+                    <div className="col-md-7">
+                        <h6 className="text-secondary fst-italic">SOME CONTENT ABOUT GALAXYCRAFT</h6>
+                        <h2 className="fw-bold text-success fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+                        <p className="text-secondary fst-italic">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate ipsam, dolorum sint quisquam facilis, aliquam numquam ducimus ad ab eaque autem! Blanditiis alias ipsum nisi velit aliquid maiores iure repellat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel magni quas minus aperiam dolor, voluptatibus corrupti nesciunt natus obcaecati ducimus! </p>
+                        <button className="btn btn-dark">Explore More</button>
+                    </div>
+
+                    <div className="col-md-5 text-center">
+                        <img src={banner} width="90%" className="img-fluid" alt="" />
+                    </div>
+                </div>
+            </Container>
+        </>
     );
 };
 
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../../Shared/Header/Header';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Banner></Banner>
+            <hr />
         </div>
     );
 };
