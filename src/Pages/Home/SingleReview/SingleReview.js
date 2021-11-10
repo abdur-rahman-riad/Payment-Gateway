cfd57b5eb2a02055f7a23d7ad9af41067e94e7ec
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-11-10 17:46:00 +0600

    Project Setup and Responsive Header

---
+++ src/Pages/Home/SingleReview/SingleReview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleReview = () => {
+    return (
+        <div>
+            <h2>Single Review</h2>
+        </div>
+    );
+};
+
+export default SingleReview;
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logo.png';
+import avatar from '../../../images/avatar.png';
+
+const Header = () => {
+
+
+    return (
+        <>
+            <Navbar className="shadow-sm" sticky="top" bg="light" variant="light" collapseOnSelect expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img src={logo}
+                            width="120"
+                            height="60"
+                            alt="" />
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle />
+
+                    <Navbar.Collapse className="justify-content-end">
+
+                        <Nav.Link className="fw-bold text-dark" as={Link} to="/login">Dashboard</Nav.Link>
+                        <Nav.Link className="fw-bold text-dark" as={Link} to="/login">Logout</Nav.Link>
+
+                        <Navbar.Text className="ms-3 d-flex mt-2">
+                            <img src={avatar}
+                                width="28"
+                                height="28" alt="" />
+                            <h6 className="mt-1 ms-2">Riad Rahman</h6>
+                        </Navbar.Text>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
